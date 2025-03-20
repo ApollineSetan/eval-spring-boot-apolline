246f5f7a52f62a44e470545a195d191f85a278fe
ApollineSetan <setan.apolline@gmail.com>
2025-03-20 14:05:49 +0100

    Add endpoint tests and API documentation

--- src/main/java/com/example/apolline/evalspring/service/ProduitService.java
+++ src/main/java/com/example/apolline/evalspring/service/ProduitService.java
@@ -1,9 +1,12 @@
 package com.example.apolline.evalspring.service;
 
+import com.example.apolline.evalspring.exception.CategorieNotFoundException;
 import com.example.apolline.evalspring.exception.NoProduitFoundException;
 import com.example.apolline.evalspring.exception.ProduitAlreadyExistsException;
 import com.example.apolline.evalspring.exception.ProduitNotFoundException;
+import com.example.apolline.evalspring.model.Categorie;
 import com.example.apolline.evalspring.model.Produit;
+import com.example.apolline.evalspring.repository.CategorieRepository;
 import com.example.apolline.evalspring.repository.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +16,18 @@ public class ProduitService {
 
     @Autowired
     private ProduitRepository produitRepository;
+    @Autowired
+    private CategorieRepository categorieRepository;
 
     // Ajouter un produit
     public Produit addProduit(Produit produit) {
         if (produitRepository.existsByNom(produit.getNom())) {
             throw new ProduitAlreadyExistsException(produit);
         }
+        // Récupére le libellé de la catégorie depuis la base de données lors de la requête et non pas seulement l'ID et l'assigne correctement
+        Categorie categorie = categorieRepository.findById(produit.getCategorie().getId())
+                .orElseThrow(() -> new CategorieNotFoundException(produit.getCategorie().getId()));
+        produit.setCategorie(categorie);
         return produitRepository.save(produit);
     }
 
